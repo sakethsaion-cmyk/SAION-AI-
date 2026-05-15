5b8bcc2f2d6f9ae10fbf614009b0fee9b98d939e
Saion <sakethsaion@gmail.com>
2026-05-15 11:34:22 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,10 +6,19 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   android: {
     allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: false,
   },
   server: {
     androidScheme: 'https',
     cleartext: true,
+    hostname: 'saion-ai.firebaseapp.com',
+  },
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: false,
+      providers: ['google.com', 'github.com'],
+    },
   },
 };
 
