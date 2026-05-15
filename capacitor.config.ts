20004e712b5229c0033619065ae7886fdf254e57
Saion <sakethsaion@gmail.com>
2026-05-15 11:14:15 +0530

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.saionproduction.saionai',
   appName: 'SAION AI',
-  webDir: 'dist'
+  webDir: 'dist',
+  android: {
+    allowMixedContent: true,
+  },
+  server: {
+    androidScheme: 'https',
+    cleartext: true,
+  },
 };
 
 export default config;
